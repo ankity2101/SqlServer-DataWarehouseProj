b3b39382f83d1bb8fb636262ceb3a38099f32d23
Ankit <102505154+ankity2101@users.noreply.github.com>
2025-05-24 19:44:43 +0530

    Create ddl_bronze.sql

---
+++ scripts/bronze/ddl_bronze.sql
@@ -0,0 +1,81 @@
+/*
+===========================================================================
+DDL Script : Create Bronze tables
+===========================================================================
+Purpose:
+  This script creates table in 'bronze' schema , dropping existing tables
+  if they already exists.
+  run this script to redefine the DDL structure of 'bronze' tables.
+===========================================================================
+*/
+
+
+
+if object_id ('bronze.crm_cust_info','U') is not null
+	drop table bronze.crm_cust_info;
+-- Customer info DDL script
+create table bronze.crm_cust_info(
+  cst_id INT,
+  cst_key NVARCHAR(50),
+  cst_firstname NVARCHAR(50),
+  cst_lastname NVARCHAR(50),
+  cst_marital_status NVARCHAR(50),
+  cst_gndr NVARCHAR(50),
+  cst_create_date DATE
+)
+
+if object_id ('bronze.crm_prd_info','U') is not null
+	drop table bronze.crm_prd_info;
+--prd_info DDL script
+create table bronze.crm_prd_info(
+  prd_id INT,
+  prd_key NVARCHAR(50),
+  prd_nm NVARCHAR(50),
+  prd_cost INT,
+  prd_line NVARCHAR(50),
+  prd_start_dt DATETIME,
+  prd_end_dt DATETIME
+);
+
+if object_id ('bronze.crm_sales_details','U') is not null
+	drop table bronze.crm_sales_details;
+-- sls_ord_num ddl script
+create table bronze.crm_sales_details(
+  sls_ord_num NVARCHAR(50),
+  sls_prd_key NVARCHAR(50),
+  sls_cust_id INT,
+  sls_order_dt INT,
+  sls_ship_dt INT,
+  sls_due_dt INT,
+  sls_sales INT,
+  sls_quantity INT,
+  sls_price INT
+)
+
+if object_id ('bronze.erp_cust_az12','U') is not null
+	drop table bronze.erp_cust_az12;
+--cust_az12 ddl script
+create table bronze.erp_cust_az12(
+  cid NVARCHAR(50),
+  bdate DATE,
+  gen NVARCHAR(50)
+)
+
+if object_id ('bronze.erp_loc_a101','U') is not null
+	drop table bronze.erp_loc_a101;
+----loc_a101 ddl script
+create table bronze.erp_loc_a101(
+  cid NVARCHAR(50),
+  cntry NVARCHAR(50)
+);
+
+if object_id ('bronze.erp_px_cat_g1v2','U') is not null
+	drop table bronze.erp_px_cat_g1v2;
+-- px_cat_g1v2
+create table bronze.erp_px_cat_g1v2(
+  id NVARCHAR(50),
+  cat NVARCHAR(50),
+  subcat NVARCHAR(50),
+  maintenance NVARCHAR(50)
+)
+
